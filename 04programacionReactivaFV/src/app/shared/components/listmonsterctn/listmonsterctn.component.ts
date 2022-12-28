f3403f6672103c8b593e62e388d00874f81cb153
Fermin Vazquez <fv.shutter@gmail.com>
2022-12-28 01:06:58 -0300

    .

---
+++ 04programacionReactivaFV/src/app/shared/components/listmonsterctn/listmonsterctn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listmonsterctn',
+  templateUrl: './listmonsterctn.component.html',
+  styleUrls: ['./listmonsterctn.component.scss']
+})
+export class ListmonsterctnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
