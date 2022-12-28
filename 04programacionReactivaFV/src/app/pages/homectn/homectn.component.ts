0d4ff42bc8f393d05fa4a3ddfa95e0a8adb02602
Fermin Vazquez <fv.shutter@gmail.com>
2022-12-28 12:43:34 -0300

    .

--- 04programacionReactivaFV/src/app/pages/homectn/homectn.component.ts
+++ 04programacionReactivaFV/src/app/pages/homectn/homectn.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homectn',
   templateUrl: './homectn.component.html',
-  styleUrls: ['./homectn.component.scss']
+  styleUrls: ['./homectn.component.css'],
 })
 export class HomectnComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
