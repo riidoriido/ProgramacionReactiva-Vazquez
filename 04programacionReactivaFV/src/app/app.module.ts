fc3177504cc462265eb2e69b9fb758e9726486b0
Fermin Vazquez <fv.shutter@gmail.com>
2022-12-28 13:23:56 -0300

    y bueno

--- 04programacionReactivaFV/src/app/app.module.ts
+++ 04programacionReactivaFV/src/app/app.module.ts
@@ -7,20 +7,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomectnComponent } from './pages/homectn/homectn.component';
 import { MonstersctnComponent } from './shared/components/monstersctn/monstersctn.component';
 import { ListmonsterctnComponent } from './shared/components/listmonsterctn/listmonsterctn.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomectnComponent,
     MonstersctnComponent,
-    ListmonsterctnComponent
+    ListmonsterctnComponent,
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
