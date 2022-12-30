c789d2d4c42a5bc0375fd28bc4c1ef2052feb54c
Fermin Vazquez <fv.shutter@gmail.com>
2022-12-29 22:20:28 -0300

    Update homectn.component.ts

--- 04programacionReactivaFV/src/app/pages/homectn/homectn.component.ts
+++ 04programacionReactivaFV/src/app/pages/homectn/homectn.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homectn.component.css'],
 })
 export class HomectnComponent implements OnInit {
+  digiArray = [
+    'Agumon',
+    'Piyomon',
+    'Tentomon',
+    'Gabumon',
+    'Patamon',
+    'Gomamon',
+    'Palmon',
+    'Shellmon',
+    'Greymon',
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
